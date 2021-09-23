0bfc30999067ccadca0dd4c4c26cf616475860a8
yannickbuunk <yannickbuunk@hotmail.nl>
2021-09-23 11:45:05 +0200

    family ties gemaakt

---
+++ f1m1/PYTB1L2FamliyTies/family ties.py
@@ -0,0 +1,11 @@
+username = "Yannick"
+leeftijd = "17"
+woonplaats = "Schagerbrug"
+afstand = "80 kilometer"
+sport = "Volleybal"
+
+print("Mijn naam is " + username)
+print("Ik ben " + leeftijd + " jaar oud")
+print("Ik woon al 16 jaar in " + woonplaats)
+print("De afstand tussen school en waar ik woon is " + afstand)
+print("Ik doe al 6 maanden " + sport)
